62975a39717d0b993218ad5c24ed32ddf089f55e
Muhammad Ahmed Hassan <mahsep27@gmail.com>
2025-10-30 02:47:50 +0500

    Update relay.js

--- api/relay.js
+++ api/relay.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const googleWebhook = "https://script.google.com/macros/s/AKfycbz9upou7YhOv_sSKjG2m0gyFrfQblK2BBUSXN52ExbxmPmYZwEqzVTta_yPunXllEgQWA/exec";
+    const googleWebhook = "https://script.google.com/macros/s/AKfycbwjVHEDIJTokLDAcnsCwRmAkwF0dAPxySfqvbWZ8DPxepzgrnwLVuIYgiPpgBPlijUUpA/exec";
 
     const response = await fetch(googleWebhook, {
       method: "POST",
